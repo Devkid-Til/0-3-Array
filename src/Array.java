8af2d78badf04237d9b2c1453dd8bf78a5e47c38
Devkid-Til <shijiaqi_develop@163.com>
2021-06-28 00:06:39 +0800

    实现Array类构造函数、状态获取、元素添加功能

--- src/Array.java
+++ src/Array.java
@@ -42,4 +42,32 @@ public class Array {
         return size == 0;
     }
 
+    /**
+     * 向所有元素后添加一个新元素
+     * @param e
+     * @throws IllegalAccessException
+     */
+    public void addLast(int e) throws IllegalAccessException {
+        add(size, e);
+    }
+    public void addFirst(int e) throws IllegalAccessException {
+        add(0, e);
+    }
+    /**
+     * 向数组中第index位置插入一个新元素
+     */
+    public void add(int index, int e) throws IllegalAccessException {
+        if (size == data.length) {
+            throw new IllegalAccessException("Add Failed. Array is full.");
+        }
+        if (index < 0 || index > size) {
+            throw new IllegalAccessException("Add Failed. Require index >= 0 and index <= size");
+        }
+        for (int i = size - 1; i >= index; i --) {
+            data[i+1] = data[i];
+        }
+        data[index] = e;
+        size ++;
+    }
+
 }
