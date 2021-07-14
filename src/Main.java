cfd8472bd8acc23cbdf24dba7ff2f9210c644764
Devkid-Til <shijiaqi_develop@163.com>
2021-07-14 19:20:44 +0800

    修改BUG：IllegalAccessException --> IllegalArgumentException

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) {
         Array<Integer> arr = new Array<>(10);
         for (int i = 0; i < 10; i++) {
             arr.addLast(i);
